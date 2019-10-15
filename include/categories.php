89c3a721f94e8a6c4c92ea80a532e6b4a0c6a5d7
ammar-oker <ammarokar12345@gmail.com>
2019-10-15 13:05:47 +0300

    first commit

---
+++ include/categories.php
@@ -0,0 +1,87 @@
+<div id="service" class="section md-padding text-right">
+        <div class="container">
+            <div class="row">
+                <div class="section-header text-center">
+                    <h2 class="title">ما هي خدماتنا</h2>
+                </div>
+
+                <div class="col-md-4 col-sm-6 ">
+                    <div class="service">
+                        <i class="fa fa-diamond"></i>
+                        <h3>الخدمة 1</h3>
+                        <p>
+                            شرح قصير عن الخدمة* شرح قصير عن الخدمة* شرح قصير عن الخدمة* شرح قصير عن الخدمة*
+                        </p>
+                        <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal1">
+                تطبيق الآن
+              </a>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="service">
+                        <i class="fa fa-rocket"></i>
+                        <h3>الخدمة 2</h3>
+                        <p>
+                            شرح قصير عن الخدمة* شرح قصير عن الخدمة* شرح قصير عن الخدمة* شرح قصير عن الخدمة*
+                        </p>
+                        <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal2">
+                تطبيق الآن
+              </a>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="service">
+                        <i class="fa fa-cogs"></i>
+                        <h3>الخدمة 3</h3>
+                        <p>
+                            شرح قصير عن الخدمة* شرح قصير عن الخدمة* شرح قصير عن الخدمة* شرح قصير عن الخدمة*
+                        </p>
+                        <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal3">
+                تطبيق الآن
+              </a>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="service">
+                        <i class="fa fa-diamond"></i>
+                        <h3>الخدمة 4</h3>
+                        <p>
+                            شرح قصير عن الخدمة* شرح قصير عن الخدمة* شرح قصير عن الخدمة* شرح قصير عن الخدمة*
+                        </p>
+                        <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal4">
+                تطبيق الآن
+              </a>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="service">
+                        <i class="fa fa-pencil"></i>
+                        <h3>الخدمة 5</h3>
+                        <p>
+                            شرح قصير عن الخدمة* شرح قصير عن الخدمة* شرح قصير عن الخدمة* شرح قصير عن الخدمة*
+                        </p>
+                        <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal5">
+                تطبيق الآن
+              </a>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="service">
+                        <i class="fa fa-flask"></i>
+                        <h3>الخدمة 6</h3>
+                        <p>
+                            شرح قصير عن الخدمة* شرح قصير عن الخدمة* شرح قصير عن الخدمة* شرح قصير عن الخدمة*
+                        </p>
+                        <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal6">
+                تطبيق الآن
+              </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ include/navbar.php
@@ -0,0 +1,53 @@
+<nav id="nav" class="navbar nav-transparent">
+            <div class="container">
+                <div class="navbar-header">
+                    <div class="navbar-brand">
+                        <a href="index.html">
+                            <img class="logo" src="img/logo.png" alt="logo" />
+                            <img class="logo-alt" src="img/logo-alt.png" alt="logo-alt" />
+                        </a>
+                    </div>
+
+                    <div id="sm-nav" class="nav-collapse">
+                        <span></span>
+                    </div>
+                </div>
+
+                <ul class="main-nav nav navbar-nav navbar-right text-right">
+                    <li>
+                        <a href="#home">الرئيسية</a>
+                    </li>
+                    <li>
+                        <a href="#about">عن الشركة</a>
+                    </li>
+                    <li class="has-dropdown">
+                        <a href="#service">خدماتنا</a>
+                        <ul class="dropdown">
+                            <li><a href="#">خدمة 1</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#agents">وكلاؤنا</a>
+                    </li>
+
+                    <li>
+                        <a href="#customers">زبائننا</a>
+                    </li>
+                    <li>
+                        <a href="#team">فريقنا</a>
+                    </li>
+                    <li>
+                        <a href="#app">حمل التطيبق الآن</a>
+                    </li>
+                    <li class="has-dropdown">
+                        <a href="#blog">المدونة</a>
+                        <ul class="dropdown">
+                            <li><a href="blog-single.html">منشور مدونة</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#contact">اتصل بنا</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
