f2e1ac693486e84a248ea84f39422e0db18002e6
ammar-oker <ammarokar12345@gmail.com>
2019-11-06 20:27:57 +0300

    Added sidebar, carousel for services and blog and fixed other issues

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 (function($) {
     "use strict";
-    $(window).on("load", function() {
-        $("#preloader")
-            .delay(600)
-            .fadeOut();
-    });
     $("body").scrollspy({ target: "#nav", offset: $(window).height() / 2 });
     $("#nav .main-nav a[href^='#']").on("click", function(e) {
         e.preventDefault();
@@ -24,33 +19,17 @@
     });
     $(window).on("scroll", function() {
         var wScroll = $(this).scrollTop();
-        wScroll > 1 ?
-            $("#nav").addClass("fixed-nav") :
-            $("#nav").removeClass("fixed-nav");
+        if (wScroll > 1) {
+            $("#nav").addClass("fixed-nav");
+            $("#navBtn").css("color", "black");
+
+        }else{
+          $("#nav").removeClass("fixed-nav");
+          $("#navBtn").css("color", "#fff");
+        }   
         wScroll > 700 ? $("#back-to-top").fadeIn() : $("#back-to-top").fadeOut();
     });
     $(".work").magnificPopup({ delegate: ".lightbox", type: "image" });
-    $("#about-slider").owlCarousel({
-        items: 1,
-        loop: true,
-        margin: 15,
-        nav: true,
-        navText: [
-            '<i class="fa fa-angle-left"></i>',
-            '<i class="fa fa-angle-right"></i>'
-        ],
-        dots: true,
-        autoplay: true,
-        animateOut: "fadeOut"
-    });
-    $("#testimonial-slider").owlCarousel({
-        loop: true,
-        margin: 15,
-        dots: true,
-        nav: false,
-        autoplay: true,
-        responsive: { 0: { items: 1 }, 992: { items: 2 } }
-    });
 })(jQuery);
 
 $("#inpt_search").on('focus', function() {
